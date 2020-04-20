f4239ab3a181dcf54cb0068f43fab7b31a1d1922
Sergio Gongora <segongora9@gmail.com>
2020-04-19 23:56:44 -0500

    flask

---
+++ app.py
@@ -0,0 +1,23 @@
+"""from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+	return "Hello World"
+
+@app.route("/david")
+def david():
+	return "Hello David"
+
+@app.route("/<string:name>")
+def hello(name):
+	name = name.capitalize()
+	return "<h1>Hello {}</h1>".format(name)
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("/index.html")
